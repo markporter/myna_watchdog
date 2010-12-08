27ee6a787693acf7379436c3cfce8409642d2ffb
dm4mark <dm4mark@788f0616-7946-11de-a588-638aa1453bf3>
2010-12-08 21:51:33 +0000

    *Added Array.first() and Array.last()
    git-svn-id: https://myna.googlecode.com/svn/trunk@147 788f0616-7946-11de-a588-638aa1453bf3

--- 1.0/web/shared/js/libOO/Array.js
+++ 1.0/web/shared/js/libOO/Array.js
@@ -1,182 +1,168 @@
 /* Class: Array 
-	Additional functions on the JS Array object    
+	Additional functions on the JS Array object		
 	
-	 
 */ 
-if (!Array.prototype.every)
-{
 /* Function: every
 	Tests whether all elements in the array pass the test implemented by the provided function.
 	
 	See:
 		https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Array/every
-*/
-  Array.prototype.every = function(fun /*, thisp*/)
-  {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw new TypeError();
-
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in this &&
-          !fun.call(thisp, this[i], i, this))
-        return false;
-    }
-
-    return true;
-  };
-}
-
-if (!Array.prototype.filter)
-{
+	*/
+	if (!Array.prototype.every)
+	{
+	
+		Array.prototype.every = function(fun /*, thisp*/)
+		{
+			var len = this.length;
+			if (typeof fun != "function")
+				throw new TypeError();
+	
+			var thisp = arguments[1];
+			for (var i = 0; i < len; i++)
+			{
+				if (i in this &&
+						!fun.call(thisp, this[i], i, this))
+					return false;
+			}
+	
+			return true;
+		};
+	}
 /* Function: filter
 	Creates a new array with all elements that pass the test implemented by the provided function.
 	
 	See:
 		https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Array/filter
-*/
-Array.prototype.filter = function(fun /*, thisp*/)
-  {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw new TypeError();
-
-    var res = new Array();
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in this)
-      {
-        var val = this[i]; // in case fun mutates this
-        if (fun.call(thisp, val, i, this))
-          res.push(val);
-      }
-    }
-
-    return res;
-  };
-}
-
-if (!Array.prototype.forEach)
-{
+	*/
+	if (!Array.prototype.filter)
+	{
+	
+		Array.prototype.filter = function(fun /*, thisp*/){
+			var len = this.length;
+			if (typeof fun != "function")
+				throw new TypeError();
+	
+			var res = new Array();
+			var thisp = arguments[1];
+			for (var i = 0; i < len; i++)
+			{
+				if (i in this)
+				{
+					var val = this[i]; // in case fun mutates this
+					if (fun.call(thisp, val, i, this))
+						res.push(val);
+				}
+			}
+	
+			return res;
+		};
+	}
 /* Function: forEach
 	Executes a provided function once per array element.
 
 	See:
 		https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Array/forEach
-*/
-Array.prototype.forEach = function(fun /*, thisp*/)
-  {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw new TypeError();
-
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in this)
-        fun.call(thisp, this[i], i, this);
-    }
-  };
-}
-
-if (!Array.prototype.indexOf)
-{
+	*/
+	if (!Array.prototype.forEach){
+		Array.prototype.forEach = function(fun /*, thisp*/){
+			var len = this.length;
+			if (typeof fun != "function")
+				throw new TypeError();
+		
+			var thisp = arguments[1];
+			for (var i = 0; i < len; i++)
+			{
+				if (i in this)
+				fun.call(thisp, this[i], i, this);
+			}
+		};
+	}
 /* Function: indexOf
 	Returns the first index at which a given element can be found in the array, or -1 if it is not present.
 
 	See:
 		https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Array/indexOf
-*/
-Array.prototype.indexOf = function(elt /*, from*/)
-  {
-    var len = this.length;
-
-    var from = Number(arguments[1]) || 0;
-    from = (from < 0)
-         ? Math.ceil(from)
-         : Math.floor(from);
-    if (from < 0)
-      from += len;
-
-    for (; from < len; from++)
-    {
-      if (from in this &&
-          this[from] === elt)
-        return from;
-    }
-    return -1;
-  };
-}
-
-if (!Array.prototype.lastIndexOf)
-{
+	*/
+	if (!Array.prototype.indexOf){
+		Array.prototype.indexOf = function(elt /*, from*/)
+			{
+			var len = this.length;
+		
+			var from = Number(arguments[1]) || 0;
+			from = (from < 0)
+				 ? Math.ceil(from)
+				 : Math.floor(from);
+			if (from < 0)
+				from += len;
+		
+			for (; from < len; from++)
+			{
+				if (from in this &&
+					this[from] === elt)
+				return from;
+			}
+			return -1;
+		};
+	}
 /* Function: lastIndexOf
 	Returns the last index at which a given element can be found in the array, or -1 if it is not present.
 
 	See:
 		https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Array/lastIndexOf
-*/
-Array.prototype.lastIndexOf = function(elt /*, from*/)
-  {
-    var len = this.length;
-
-    var from = Number(arguments[1]);
-    if (isNaN(from))
-    {
-      from = len - 1;
-    }
-    else
-    {
-      from = (from < 0)
-           ? Math.ceil(from)
-           : Math.floor(from);
-      if (from < 0)
-        from += len;
-      else if (from >= len)
-        from = len - 1;
-    }
-
-    for (; from > -1; from--)
-    {
-      if (from in this &&
-          this[from] === elt)
-        return from;
-    }
-    return -1;
-  };
-}
-
-if (!Array.prototype.map)
-{
+	*/
+	if (!Array.prototype.lastIndexOf){
+		Array.prototype.lastIndexOf = function(elt /*, from*/){
+			var len = this.length;
+		
+			var from = Number(arguments[1]);
+			if (isNaN(from))
+			{
+				from = len - 1;
+			}
+			else
+			{
+				from = (from < 0)
+					 ? Math.ceil(from)
+					 : Math.floor(from);
+				if (from < 0)
+				from += len;
+				else if (from >= len)
+				from = len - 1;
+			}
+		
+			for (; from > -1; from--)
+			{
+				if (from in this &&
+					this[from] === elt)
+				return from;
+			}
+			return -1;
+		};
+	}
 /* Function: map
 	Creates a new array with the results of calling a provided function on every element in this array.
 
 	See:
 		https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Array/map
-*/
-Array.prototype.map = function(fun /*, thisp*/)
-  {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw new TypeError();
-
-    var res = new Array(len);
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in this)
-        res[i] = fun.call(thisp, this[i], i, this);
-    }
-
-    return res;
-  };
-}
-
-if (!Array.dim)
-{
+	*/
+	if (!Array.prototype.map){
+		Array.prototype.map = function(fun /*, thisp*/)
+			{
+			var len = this.length;
+			if (typeof fun != "function")
+				throw new TypeError();
+		
+			var res = new Array(len);
+			var thisp = arguments[1];
+			for (var i = 0; i < len; i++)
+			{
+				if (i in this)
+				res[i] = fun.call(thisp, this[i], i, this);
+			}
+		
+			return res;
+		};
+	}
 /* Function: dim
 	returns an array of size _count_ containing null objects
 	
@@ -190,16 +176,17 @@ if (!Array.dim)
 			return "String " + index++; 
 		})
 	(end)
-*/
-  Array.dim = function(count)
-  {
-	  var result=[]
-	  for (var x = 0; x < count; ++x){
-			 result.push(null);
-	  }
-	  return result
-  };
-}
+	*/
+	if (!Array.dim){
+	
+		Array.dim = function(count){
+			var result=[]
+			for (var x = 0; x < count; ++x){
+				 result.push(null);
+			}
+			return result
+		};
+	}
 
 
 
@@ -208,133 +195,120 @@ if (!Array.dim)
 	
 	See:
 		https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Array/reduce
+	*/
+	if (!Array.prototype.reduce){
+		Array.prototype.reduce = function(fun /*, initial*/){
+			var len = this.length;
+			if (typeof fun != "function")
+				throw new TypeError();
 		
-*/
-if (!Array.prototype.reduce)
-{
-/* Function: reduce
-	Apply a function simultaneously against two values of the array (from left-to-right) as to reduce it to a single value.
-	
-	See:
-		https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Array/reduce
-*/
-Array.prototype.reduce = function(fun /*, initial*/)
-  {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw new TypeError();
-
-    // no value to return if no initial value and an empty array
-    if (len == 0 && arguments.length == 1)
-      throw new TypeError();
-
-    var i = 0;
-    if (arguments.length >= 2)
-    {
-      var rv = arguments[1];
-    }
-    else
-    {
-      do
-      {
-        if (i in this)
-        {
-          rv = this[i++];
-          break;
-        }
-
-        // if array contains no values, no initial value to return
-        if (++i >= len)
-          throw new TypeError();
-      }
-      while (true);
-    }
-
-    for (; i < len; i++)
-    {
-      if (i in this)
-        rv = fun.call(null, rv, this[i], i, this);
-    }
-
-    return rv;
-  };
-}
-
-if (!Array.prototype.reduceRight)
-{
+			// no value to return if no initial value and an empty array
+			if (len == 0 && arguments.length == 1)
+				throw new TypeError();
+		
+			var i = 0;
+			if (arguments.length >= 2)
+			{
+				var rv = arguments[1];
+			}
+			else
+			{
+				do
+				{
+				if (i in this)
+				{
+					rv = this[i++];
+					break;
+				}
+		
+				// if array contains no values, no initial value to return
+				if (++i >= len)
+					throw new TypeError();
+				}
+				while (true);
+			}
+		
+			for (; i < len; i++)
+			{
+				if (i in this)
+				rv = fun.call(null, rv, this[i], i, this);
+			}
+		
+			return rv;
+		};
+	}
 /* Function: reduceRight
 	Apply a function simultaneously against two values of the array (from right-to-left) as to reduce it to a single value.
 	
 	See:
 		https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Array/reduceRight
-*/
-Array.prototype.reduceRight = function(fun /*, initial*/)
-  {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw new TypeError();
-
-    // no value to return if no initial value, empty array
-    if (len == 0 && arguments.length == 1)
-      throw new TypeError();
-
-    var i = len - 1;
-    if (arguments.length >= 2)
-    {
-      var rv = arguments[1];
-    }
-    else
-    {
-      do
-      {
-        if (i in this)
-        {
-          rv = this[i--];
-          break;
-        }
-
-        // if array contains no values, no initial value to return
-        if (--i < 0)
-          throw new TypeError();
-      }
-      while (true);
-    }
-
-    for (; i >= 0; i--)
-    {
-      if (i in this)
-        rv = fun.call(null, rv, this[i], i, this);
-    }
-
-    return rv;
-  };
-}
-
-if (!Array.prototype.some)
-{
+	*/
+	if (!Array.prototype.reduceRight){
+		Array.prototype.reduceRight = function(fun /*, initial*/){
+			var len = this.length;
+			if (typeof fun != "function")
+				throw new TypeError();
+		
+			// no value to return if no initial value, empty array
+			if (len == 0 && arguments.length == 1)
+				throw new TypeError();
+		
+			var i = len - 1;
+			if (arguments.length >= 2)
+			{
+				var rv = arguments[1];
+			}
+			else
+			{
+				do
+				{
+				if (i in this)
+				{
+					rv = this[i--];
+					break;
+				}
+		
+				// if array contains no values, no initial value to return
+				if (--i < 0)
+					throw new TypeError();
+				}
+				while (true);
+			}
+		
+			for (; i >= 0; i--)
+			{
+				if (i in this)
+				rv = fun.call(null, rv, this[i], i, this);
+			}
+		
+			return rv;
+		};
+	}
 /* Function: some
 	Tests whether some element in the array passes the test implemented by the provided function.
 	
 	See:
 		https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Array/some
-*/
-Array.prototype.some = function(fun /*, thisp*/)
-  {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw new TypeError();
-
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in this &&
-          fun.call(thisp, this[i], i, this))
-        return true;
-    }
-
-    return false;
-  };
-}
+	*/
+	if (!Array.prototype.some){
+	
+		Array.prototype.some = function(fun /*, thisp*/)
+			{
+			var len = this.length;
+			if (typeof fun != "function")
+				throw new TypeError();
+		
+			var thisp = arguments[1];
+			for (var i = 0; i < len; i++)
+			{
+				if (i in this &&
+					fun.call(thisp, this[i], i, this))
+				return true;
+			}
+		
+			return false;
+		};
+	}
 
 /* Function: min
 	returns the "smallest" member of the array.
@@ -344,15 +318,15 @@ Array.prototype.some = function(fun /*, thisp*/)
 						A compare function like sort() uses to determine the minimum
 						value
 	
-*/
-Array.prototype.min = function(compare) {
-	if (!compare) compare = function(a,b){return a < b}
-	return this.reduce(function(result,e){
-		if (result === null ||compare(e,result)) {
-			return e;
-		} else return result;
-	},null);
-}
+	*/
+	Array.prototype.min = function(compare) {
+		if (!compare) compare = function(a,b){return a < b}
+		return this.reduce(function(result,e){
+			if (result === null ||compare(e,result)) {
+				return e;
+			} else return result;
+		},null);
+	}
 
 /* Function: max
 	returns the "largest" member of the array.
@@ -362,15 +336,15 @@ Array.prototype.min = function(compare) {
 						A compare function like sort() uses to determaxe the maximum
 						value
 	
-*/
-Array.prototype.max = function(compare) {
-	if (!compare) compare = function(a,b){return a > b}
-	return this.reduce(function(result,e){
-		if (result === null ||compare(e,result)) {
-			return e;
-		} else return result;
-	},null);
-}
+	*/
+	Array.prototype.max = function(compare) {
+		if (!compare) compare = function(a,b){return a > b}
+		return this.reduce(function(result,e){
+			if (result === null ||compare(e,result)) {
+				return e;
+			} else return result;
+		},null);
+	}
 
 /* Function: sum
 	returns a sum of the array.
@@ -382,13 +356,13 @@ Array.prototype.max = function(compare) {
 						to sum a property of the objects in the array rather than
 						the objects themselves.
 	
-*/
-Array.prototype.sum = function(accessor) {
-	if (!accessor) accessor = function(element){return element}
-	return this.reduce(function(result,e){
-		return result + accessor(e);
-	},0);
-}
+	*/
+	Array.prototype.sum = function(accessor) {
+		if (!accessor) accessor = function(element){return element}
+		return this.reduce(function(result,e){
+			return result + accessor(e);
+		},0);
+	}
 
 /* Function: avg
 	returns an average of the array.
@@ -408,21 +382,46 @@ Array.prototype.sum = function(accessor) {
 				return element===null?0:element;
 			}
 		(end)
-*/
-Array.prototype.avg = function(accessor) {
-	if (!accessor) accessor = function(element){return element}
-	return this.filter(function(e){
-		return accessor(e) !== null;
-	}).reduce(function(result,e,index,array){
-		if (index < array.length -1){
-			return result + accessor(e);
-		} else {
-			result += accessor(e);
-			return result / array.length;
-		}
-	},0);
-}
-
+	*/
+	Array.prototype.avg = function(accessor) {
+		if (!accessor) accessor = function(element){return element}
+		return this.filter(function(e){
+			return accessor(e) !== null;
+		}).reduce(function(result,e,index,array){
+			if (index < array.length -1){
+				return result + accessor(e);
+			} else {
+				result += accessor(e);
+				return result / array.length;
+			}
+		},0);
+	}
+/* Function: last
+	returns the last value in this array
+	
+	Parameters:
+		defaultValue	-	*Optional, default throws error*
+							If defined, this value will be returned if the 
+							Array is empty. Otherwise an error is thrown
+	*/
+	Array.prototype.last = function(){
+		if (this.length) return this[this.length-1]
+		if (arguments.length) return arguments[0]
+		throw new Error("Attempt to call last() on empty Array without specifying a defaultValue") 
+	}
+/* Function: first
+	returns the first value in this array
+	
+	Parameters:
+		defaultValue	-	*Optional, default throws error*
+							If defined, this value will be returned if the 
+							Array is empty. Otherwise an error is thrown
+	*/
+	Array.prototype.first = function(){
+		if (this.length) return this[0]
+		if (arguments.length) return arguments[0]
+		throw new Error("Attempt to call first() on empty Array without specifying a defaultValue") 
+	}
 
 /* Function: parse 
 	Static function that returns an array from an array like object, or null if 
@@ -457,17 +456,17 @@ Array.prototype.avg = function(accessor) {
 		})
 	)
 	(end)
-*/
-Array.parse = function ParseArray(obj,accessFunction,lengthFunction){
-	if (!accessFunction) accessFunction = function(obj,index){return obj[index]}
-	if (!lengthFunction) lengthFunction = function(obj){return obj.length}
-	
-	var result =[];
-	for (var x=0; x < lengthFunction(obj); ++x){
-		result.push(accessFunction(obj,x));	
+	*/
+	Array.parse = function ParseArray(obj,accessFunction,lengthFunction){
+		if (!accessFunction) accessFunction = function(obj,index){return obj[index]}
+		if (!lengthFunction) lengthFunction = function(obj){return obj.length}
+		
+		var result =[];
+		for (var x=0; x < lengthFunction(obj); ++x){
+			result.push(accessFunction(obj,x));	
+		}
+		return result;
 	}
-	return result;
-}
 
 /* Function: compact 
 	removes undefined values from an array
@@ -488,13 +487,13 @@ Array.parse = function ParseArray(obj,accessFunction,lengthFunction){
 		a.compact();
 		Myna.println(a.length);//prints 25
 	(end)
-*/
-Array.prototype.compact = function compactArray(){
-	var a =this;
-	for (var i = a.length-1;i >=0;--i){
-		if (a[i] === undefined) a.splice(i,1);
+	*/
+	Array.prototype.compact = function compactArray(){
+		var a =this;
+		for (var i = a.length-1;i >=0;--i){
+			if (a[i] === undefined) a.splice(i,1);
+		}
 	}
-}
 
 /* Function: contains 
 	searches an array and returns true if one or more are found
@@ -530,17 +529,17 @@ Array.prototype.compact = function compactArray(){
 		})
 		
 	(end)
-*/
-Array.prototype.contains = function contains(search){
-	for (var x=0; x<this.length;++x){
-		if (typeof search === "function"){
-			if (search(this[x])) return true;	
-		} else {
-			if (this[x] == search) return true	
+	*/
+	Array.prototype.contains = function contains(search){
+		for (var x=0; x<this.length;++x){
+			if (typeof search === "function"){
+				if (search(this[x])) return true;	
+			} else {
+				if (this[x] == search) return true	
+			}
 		}
+		return false;
 	}
-	return false;
-}
 
 if ("$server_gateway" in this){
 	(function(){
